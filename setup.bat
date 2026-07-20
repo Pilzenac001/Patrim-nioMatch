0f056dff19211a59759d2ce1f9d5ac7cce563db3
Pilzenac001 <machorip2@gmail.com>
2026-07-19 21:34:08 -0300

    Atualiza scripts de setup e rodar para compatibilidade

--- setup.bat
+++ setup.bat
@@ -1,10 +1,16 @@
 @echo off
 echo Criando ambiente virtual...
 python -m venv venv
-echo Ativando ambiente e instalando dependencias...
 call venv\Scripts\activate
+
+echo Instalando dependencias...
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-echo.
-echo SETUP CONCLUIDO! Voce ja pode usar o rodar.bat
+
+echo Criando pastas de trabalho...
+if not exist "entrada" mkdir entrada
+if not exist "saida" mkdir saida
+if not exist "logs" mkdir logs
+
+echo SETUP CONCLUIDO!
 pause
